d9a95a5bbbe5248b9b880dc8c06baaf3af0f6e27
nyatado98 <danndong080@gmail.com>
2023-12-04 10:14:05 +0300

    Few changes on the readme file

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 include 'students/database/connect.php';
 if(isset($_SESSION["user_email"]) || $_SESSION["email_user"] == true){
@@ -6,15 +5,4 @@ if(isset($_SESSION["user_email"]) || $_SESSION["email_user"] == true){
 	exit;
 }
 require('students/dashboard.php');
-
-=======
-<?php 
-include 'students/database/connect.php';
-if(isset($_SESSION["user_email"]) || $_SESSION["email_user"] == true){
-	header("location:students/dashboard");
-	exit;
-}
-require('students/dashboard.php');
-
->>>>>>> 2019fb6bd2dd2ea15b95e71ca458d606c33ff2c1
 ?>
