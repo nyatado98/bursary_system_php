307956437128da687b8e8c3f98ad5af6d1a6bb38
Vincent Kiplagat <kiplagatvincent5@gmail.com>
2023-12-14 18:53:01 +0300

    Changed Phone No : to Phone Number :

--- students/application.php
+++ students/application.php
@@ -726,7 +726,7 @@ body{
                                                     </div>
                                                     <div class="col-md-4">
 
-                                                        <label class="font-weight-bold" for="phone">Phone No :</label>
+                                                        <label class="font-weight-bold" for="phone">Phone Number :</label>
                                                         <input type="number" name="phone" id="phone" class="form-control font-weight-bold <?php echo $phone_err ? 'border border-danger' : '';?>" placeholder="07 - - - - - -" value="<?php echo $phone;?>" oninput="validatePhoneNumber()">
                                                         
                                                         <span class="font-italic">Example:0712345678</span><br>
